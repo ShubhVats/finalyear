fabceb702a3a1d29889c17b4a8390b314a6122c4
ShubhVats <78045429+ShubhVats@users.noreply.github.com>
2022-02-20 13:37:02 +0530

    Review Post Updated

--- routes/Reviews.js
+++ routes/Reviews.js
@@ -2,9 +2,28 @@ const router = require("express").Router();
 let Review = require("../models/Review.model");
 
 router.route("/").get((req, res) => {
-  Review.find()
-    .then((review) => res.json(review))
-    .catch((err) => res.status(400).json("Error: " + err));
+    Review.find()
+        .then((review) => res.json(review))
+        .catch((err) => res.status(400).json("Error: " + err));
+});
+
+router.route("/add").post((req, res) => {
+    const name = req.body.name;
+    const profession = req.body.profession;
+    const review = req.body.review;
+    const stars = req.body.stars;
+
+    const newReview = new Review({
+        name,
+        profession,
+        review,
+        stars,
+    });
+
+    newReview
+        .save()
+        .then(() => res.json("Test Added!"))
+        .catch((err) => res.status(400).json("Ërror: " + err));
 });
 
 module.exports = router;
